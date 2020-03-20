b7319603006e2eb03522559913b2fe61155f4185
Richard Cook <rcook@rcook.org>
2020-03-19 21:05:29 -0700

    Add Git logging

--- probe.ps1
+++ probe.ps1
@@ -9,6 +9,26 @@
 [CmdletBinding()]
 param()
 
+function invoke() {
+    param()
+
+    if ($args.Count -eq 0) {
+        throw 'Must supply some arguments'
+    }
+
+    $command = $Args[0]
+    $commandArgs = @()
+    if ($Args.Count -gt 1) {
+        $commandArgs = $Args[1..($Args.Count - 1)]
+    }
+
+    & $command $commandArgs
+    $result = $LastExitCode
+    if ($result -ne 0) {
+        throw "$command $commandArgs failed with exit status $result"
+    }
+}
+
 $ErrorActionPreference = 'Stop'
 
 $thisDir = $PSScriptRoot
@@ -27,3 +47,7 @@ Write-Output "artifactsDir=$artifactsDir" | Tee-Object -FilePath $artifactsDir\v
 Get-ChildItem -Recurse -Path $thisDir | Select-Object FullName | Tee-Object -FilePath $artifactsDir\this-dir.txt
 Get-ChildItem -Recurse -Path $currentDir | Select-Object FullName | Tee-Object -FilePath $artifactsDir\current-dir.txt
 Get-ChildItem -Path Env: | Tee-Object -FilePath $artifactsDir\env.txt
+
+invoke git log --oneline | Tee-Object -FilePath $artifactsDir\git-log.txt
+invoke git tag | ForEach-Object { Write-Output "$_ $(invoke git rev-list -n 1 $_)" } | Tee-Object -FilePath $artifactsDir\git-tags.txt
+invoke git branch -vv -a | Tee-Object -FilePath $artifactsDir\git-branches.txt
