b3f2909897d68e54b7dde1e70d4dc394532ac86c
Richard Cook <rcook@rcook.org>
2020-03-20 15:38:24 -0700

    Bootstrap

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,1 +1,2 @@
-$env:FOO = 'hello-world'
+$env:MYCARGOBIN = Resolve-Path -Path "$(if ($env:USERPROFILE -ne $null) { $env:USERPROFILE } else { $env:HOME })\.cargo\bin"
+$env:PATH = $env:PATH + [System.IO.Path]::PathSeparator + $env:MYCARGOBIN
