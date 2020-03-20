f1109391721375a9af346e45aea244666edffb82
Richard Cook <rcook@rcook.org>
2020-03-19 21:22:53 -0700

    Log everything to stdout

--- probe.ps1
+++ probe.ps1
@@ -33,21 +33,41 @@ $ErrorActionPreference = 'Stop'
 
 $thisDir = $PSScriptRoot
 $currentDir = Get-Location
-$artifactsDir = Join-Path -Path $thisDir -ChildPath _artifacts
-New-Item -ErrorAction Ignore -ItemType Directory -Path $artifactsDir | Out-Null
-$artifactsDir = Resolve-Path -Path $artifactsDir
 
-Write-Output "IsLinux=$IsLinux" | Tee-Object -FilePath $artifactsDir\vars.txt
-Write-Output "IsMacOS=$IsMacOS" | Tee-Object -FilePath $artifactsDir\vars.txt -Append
-Write-Output "IsWindows=$IsWindows" | Tee-Object -FilePath $artifactsDir\vars.txt -Append
-Write-Output "thisDir=$thisDir" | Tee-Object -FilePath $artifactsDir\vars.txt -Append
-Write-Output "currentDir=$currentDir" | Tee-Object -FilePath $artifactsDir\vars.txt -Append
-Write-Output "artifactsDir=$artifactsDir" | Tee-Object -FilePath $artifactsDir\vars.txt -Append
+Write-Output "IsLinux: $IsLinux"
+Write-Output "IsMacOS: $IsMacOS"
+Write-Output "IsWindows: $IsWindows"
+Write-Output "thisDir: $thisDir"
+Write-Output "currentDir: $currentDir"
 
-Get-ChildItem -Recurse -Path $thisDir | Select-Object FullName | Tee-Object -FilePath $artifactsDir\this-dir.txt
-Get-ChildItem -Recurse -Path $currentDir | Select-Object FullName | Tee-Object -FilePath $artifactsDir\current-dir.txt
-Get-ChildItem -Path Env: | Tee-Object -FilePath $artifactsDir\env.txt
+Write-Output "Files under $($thisDir):"
+Get-ChildItem -Recurse -Path $thisDir | Sort-Object FullName | ForEach-Object {
+    Write-Output "  $($_.FullName)"
+}
 
-invoke git log --oneline | Tee-Object -FilePath $artifactsDir\git-log.txt
-invoke git tag | ForEach-Object { Write-Output "$_ $(invoke git rev-list -n 1 $_)" } | Tee-Object -FilePath $artifactsDir\git-tags.txt
-invoke git branch -vv -a | Tee-Object -FilePath $artifactsDir\git-branches.txt
+if ($thisDir -ne $currentDir) {
+    Write-Output "Files under $($currentDir):"
+    Get-ChildItem -Recurse -Path $currentDir | Sort-Object FullName | ForEach-Object {
+        Write-Output "  $($_.FullName)"
+    }
+}
+
+Write-Output 'Environment:'
+Get-ChildItem -Path Env: | Sort-Object Key | ForEach-Object {
+    Write-Output "  $($_.Key) = $($_.Value)"
+}
+
+Write-Output 'Git log:'
+invoke git log --oneline --color=never | ForEach-Object {
+    Write-Output "  $_"
+}
+
+Write-Output 'Git tags:'
+invoke git tag | ForEach-Object {
+    Write-Output "  $_ $(invoke git rev-list -n 1 $_)"
+}
+
+Write-Output 'Git branches:'
+invoke git branch -vv -a --color=never | ForEach-Object {
+    Write-Output "  $_"
+}
